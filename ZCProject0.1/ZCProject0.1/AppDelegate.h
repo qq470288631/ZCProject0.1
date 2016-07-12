3d7ed5b688ede7caf706b2a83b8d4160a9604e5a
tangxu <kingtangxu@163.com>
2016-07-12 16:06:35 +0800

    项目搭建（目录部分）

---
+++ ZCProject0.1/ZCProject0.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCProject0.1
+//
+//  Created by 唐旭 on 16/7/12.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCProject0.1/ZCProject0.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZCProject0.1
+//
+//  Created by 唐旭 on 16/7/12.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
