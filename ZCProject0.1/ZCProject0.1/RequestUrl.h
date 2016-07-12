5373dbbd9ef8e12f5321a25a614d7e8cae2d21c3
tangxu <kingtangxu@163.com>
2016-07-12 20:07:55 +0800

    添加 宏定义 PCH文件 网络请求路径文件

---
+++ ZCProject0.1/ZCProject0.1/RequestUrl.h
@@ -0,0 +1,25 @@
+//
+//  RequestUrl.h
+//  DouBanDemo
+//
+//  Created by 唐旭 on 16/6/27.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#ifndef RequestUrl_h
+#define RequestUrl_h
+
+
+
+/**
+ *  活动详情url（样例）
+ */
+#define ActivityDetailRequest_Url(ID) [NSString stringWithFormat:@"http://api.douban.com/v2/event/%@/photos?count=5&apikey=062bcf31694a52d212836d943bdef876",ID]
+
+
+
+
+
+
+
+#endif /* RequestUrl_h */
