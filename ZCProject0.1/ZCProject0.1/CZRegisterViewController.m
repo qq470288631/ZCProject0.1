4ddca77dc0b04b74db305eafbfe0441342b2a929
tangxu <kingtangxu@163.com>
2016-07-13 20:43:10 +0800

    替换登录、注册界面文件

---
+++ ZCProject0.1/ZCProject0.1/CZRegisterViewController.m
@@ -0,0 +1,37 @@
+//
+//  CZRegisterViewController.m
+//  ZCProject0.1
+//
+//  Created by 唐旭 on 16/7/13.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#import "CZRegisterViewController.h"
+
+@interface CZRegisterViewController ()
+
+@end
+
+@implementation CZRegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
