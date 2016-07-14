09a8eb0818f7227fe74a90a93274367ea146689d
tangxu <kingtangxu@163.com>
2016-07-14 11:31:53 +0800

    增加User控制类

---
+++ ZCProject0.1/ZCProject0.1/ProvingLoginStatus.h
@@ -0,0 +1,13 @@
+//
+//  ProvingLoginStatus.h
+//  ZCProject0.1
+//
+//  Created by 唐旭 on 16/7/14.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProvingLoginStatus : NSObject
+
+@end
---
+++ ZCProject0.1/ZCProject0.1/ProvingLoginStatus.m
@@ -0,0 +1,13 @@
+//
+//  ProvingLoginStatus.m
+//  ZCProject0.1
+//
+//  Created by 唐旭 on 16/7/14.
+//  Copyright © 2016年 唐旭. All rights reserved.
+//
+
+#import "ProvingLoginStatus.h"
+
+@implementation ProvingLoginStatus
+
+@end
